c774110306b265dfde2a0379a042d1ebe954d13d
weiyuyungx <52741575@qq.com>
2020-12-07 11:45:59 +0800

    修改baseService

--- app/base/BaseService.php
+++ app/base/BaseService.php
@@ -2,21 +2,103 @@
 namespace app\base;
 
 use app\libary\Util;
+use Phalcon\Mvc\Model;
 
 
 /**
  *
  * @author Administrator
- *        
+ *
  */
 abstract class BaseService
 {
     
-    abstract static function findOneByid($id);
+    /**
+     * #查找一条
+     * @author  WYY 2020-11-16 09:44
+     * @param int $id
+     * @param bool $throwable 为空时处理
+     * @throws \Exception
+     * @return Model
+     */
+    public static function findOneByid($id , $throwable = false)
+    {
+        $class = Util::serviceNameToModelName(static::class);
+        
+        
+        $builder = new \Phalcon\Mvc\Model\Query\Builder();
+        
+        $builder->from($class);
+        $builder->where('id = :id:',['id'=>$id]);
+        
+        $model =  $builder->getQuery()->setUniqueRow(true)->execute();
+        
+        if ($model)
+            return $model;
+            else {
+                if ($throwable == true) {
+                    Util::throwException(11, '不存在的ID');
+                }
+                else {
+                    return null;
+                }
+            }
+            
+    }
     
-    abstract static function saveOne($data, $id = 0, $white_list = null); 
-
-    abstract static function delOne($id); 
+    
+    /**
+     * #保存一条
+     * @author  WYY 2020-11-13 17:54
+     * @param array $data
+     * @param number $id
+     * @param array $white_list
+     * @throws \Exception
+     * @return boolean
+     */
+    public static function saveOne($data, $id = 0, $white_list = null)
+    {
+        if ($id > 0)
+        {
+            $model = self::findOneByid($id,true);
+        }
+        else
+        {
+            //生成一个新对象
+            $model = static::getNewModel();
+        }
+        
+        return $model->save($data,$white_list);
+    }
+    
+    
+    
+    /**
+     * #删除一条
+     * @author  WYY 2020-11-13 17:43
+     * @param int $id
+     * @return boolean
+     */
+    public static function delOne($id)
+    {
+        return self::findTrueOne($id)->delete();
+    }
+    
+    
+    
+    /**
+     * #生成一个新Model
+     * @author  WYY 2020-11-16 09:53
+     * @return Model
+     */
+    public static function getNewModel()
+    {
+        $class = Util::serviceNameToModelName(static::class);
+        
+        return new $class();
+    }
+    
+    
     
 }
 
--- app/view/helper/service.tpl
+++ app/view/helper/service.tpl
@@ -3,74 +3,29 @@
 namespace {{name_space}};
 
 use {{self_dao}} as SelfDao;
-use {{full_model_name}};
+use {{full_model_name}} as SelfModel;
 use app\libary\Util;
 
 /**
  * 
- *  @author {{author}}    
+ *  @author {{author}}  
  */
 class {{service_name}} extends {{parent_class}}
 
 {
 
+   
     /** 
-          *  查找一条
-     * @param  $id
-     * @return {{model_name}}
-     
-     */
-    public static function findOneByid($id)
-    {
-        return SelfDao::findOne($id);
-    }
-    
-    /** 
-          *  保存一条
-     * @param array $data 要保存的数据
-     * @param number $id  大于0表示修改
-     * @param array $white_list  白名单
-     * @throws \Exception
-     * @return number|boolean
-     */
-    public static function saveOne($data, $id = 0 ,$white_list = null)
-    {
-        return SelfDao::saveOne($data, $id , $white_list);
-    }
-    
-    
-    /** 
-          *   删除一条
-     * @param  $id
-     * @return boolean
-     */
-    public static function delOne($id)
-    {
-        return SelfDao::delOne($id);
-    }
-    
-
-    /** 
-          * 查找一条已经存在的记录
+     * #查找一条
+     * @author  WYY 2020-11-16 15:53
      * @param int $id
-     * @return {{model_name}}   
-     * @throws \Exception
+     * @param boolean $throwable 为空时是否抛错
+     * @return SelfModel
      */
-    public static  function findTrueOne($id) 
+    public static function findOneByid($id , $throwable = false) 
     {
-        $one = static::findOneByid($id);
-        
-        if (empty($one))
-            Util::throwException(11, '无效的id');
-       
-        return $one;    
+        return parent::findOneByid($id ,$throwable);
     }
-    
-    
-    
-    
-    
-    
     
     
     
